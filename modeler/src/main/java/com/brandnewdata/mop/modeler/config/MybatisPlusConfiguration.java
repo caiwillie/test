d1846160bdb4b70365e1c80ac9244ad4229da092
蔡尉立 <caiwillie@163.com>
2022-06-09 13:21:32 +0800

    新增配置

---
+++ modeler/src/main/java/com/brandnewdata/mop/modeler/config/MybatisPlusConfiguration.java
@@ -0,0 +1,92 @@
+package com.brandnewdata.mop.modeler.config;
+
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * @author caiwillie
+ */
+@Configuration("modelMybatisPlusConfiguration")
+public class MybatisPlusConfiguration {
+    // private static final String LOCATION_PATTERN = "classpath*:com/shinemo/suc/core/mappers/DataDao.xml";
+
+    @Bean("modelMainMapperScannerConfigurer")
+    public static MapperScannerConfigurer mapperScannerConfigurer() throws Exception {
+        MapperScannerConfigurer ret = new MapperScannerConfigurer();
+        ret.setSqlSessionFactoryBeanName("coreMainSqlSessionFactoryBean");
+        ret.setBasePackage("com.shinemo.suc.core.dao.mybatis");
+        return ret;
+    }
+
+    @Bean("coreMainSqlSessionFactoryBean")
+    public static SqlSessionFactory sqlSessionFactoryBean(
+            DataSource dataSource,
+            ResourcePatternResolver resourcePatResolver,
+            @Value("${sm.schema}") String schema) throws Exception {
+        MybatisSqlSessionFactoryBean factory = new MybatisSqlSessionFactoryBean();
+        factory.setDataSource(dataSource);
+
+        // factory.setConfiguration(null); 原生mybatis相关配置
+        factory.setGlobalConfig(globalConfig(schema)); // mybatis plus新增的相关配置
+        // factory.setPlugins(mybatisPlusInterceptor()); // mybatis plus插件相关配置
+        // factory.setMapperLocations(resourcePatResolver.getResources(LOCATION_PATTERN));
+        return factory.getObject();
+    }
+
+    private static GlobalConfig globalConfig(String schema) {
+        GlobalConfig config = new GlobalConfig();
+
+        // 数据库相关的配置
+        GlobalConfig.DbConfig dbConfig = new GlobalConfig.DbConfig();
+        dbConfig.setSchema(schema);
+        config.setDbConfig(dbConfig);
+
+        // 钩子处理器
+        config.setMetaObjectHandler(new MetaObjectHandler() {
+            @Override
+            public void insertFill(MetaObject metaObject) {
+                this.strictInsertFill(metaObject, "created", LocalDateTime.class, LocalDateTime.now());
+                this.strictInsertFill(metaObject, "lastUpdated", LocalDateTime.class, LocalDateTime.now());
+            }
+
+            @Override
+            public void updateFill(MetaObject metaObject) {
+                this.strictUpdateFill(metaObject, "lastUpdated", LocalDateTime.class, LocalDateTime.now());
+            }
+
+        });
+        return config;
+    }
+
+    private static MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor ret = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor =
+                new PaginationInnerInterceptor();
+        OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor =
+                new OptimisticLockerInnerInterceptor();
+        ret.setInterceptors(Arrays.asList(
+                paginationInnerInterceptor,
+                optimisticLockerInnerInterceptor));
+        return ret;
+    }
+
+
+}
