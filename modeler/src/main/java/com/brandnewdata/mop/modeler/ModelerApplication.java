38b56600d9af5d2c9af3f1313d3d45f669d08c75
蔡尉立 <caiwillie@163.com>
2022-06-09 10:07:43 +0800

    更新配置

---
+++ modeler/src/main/java/com/brandnewdata/mop/modeler/ModelerApplication.java
@@ -0,0 +1,12 @@
+package com.brandnewdata.mop.modeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelerApplication.class, args);
+	}
+}
