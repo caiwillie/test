adb60db19d7583850b02ea046018b52c4e3164d8
蔡尉立 <caiwillie@163.com>
2022-06-10 14:53:36 +0800

    新增worker示例

--- poc/src/main/java/com/brandnewdata/mop/modeler/PocApplication.java
+++ poc/src/main/java/com/brandnewdata/mop/modeler/PocApplication.java
@@ -3,10 +3,11 @@ package com.brandnewdata.mop.modeler;
 import io.camunda.zeebe.spring.client.EnableZeebeClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-// https://github.com/camunda-community-hub/spring-zeebe/
-@EnableZeebeClient
 
+@EnableZeebeClient // https://github.com/camunda-community-hub/spring-zeebe/
+@RefreshScope // https://nacos.io/zh-cn/docs/quick-start-spring-cloud.html
 @SpringBootApplication
 public class PocApplication {
 	public static void main(String[] args) {
