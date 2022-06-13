c5d0097e19dbd1604c7f1ff5419585d86f0fd6d2
蔡尉立 <caiwillie@163.com>
2022-06-13 14:27:51 +0800

    更新包名

--- poc/src/main/java/com/brandnewdata/mop/poc/message/MessageResource.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/message/MessageResource.java
@@ -4,9 +4,15 @@ import cn.hutool.core.lang.Assert;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.brandnewdata.mop.poc.common.service.result.Result;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
 import io.camunda.zeebe.client.ZeebeClient;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
@@ -22,9 +28,10 @@ import java.util.Map;
 @RestController
 public class MessageResource {
 
-    /**
-     * The Zeebe.
-     */
+    private static final ObjectMapper OM = new ObjectMapper();
+    private static final MapType MAP_TYPE = OM.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
+
+
     @Resource
     private ZeebeClient zeebe;
 
@@ -33,6 +40,7 @@ public class MessageResource {
      */
     @PostMapping(value = "/message/send")
     public Result sned(@RequestBody MessageDTO message) {
+
         String type = message.getType();
         String correlationKey = message.getCorrelationKey();
         String content = message.getContent();
@@ -45,16 +53,21 @@ public class MessageResource {
 
         // 解析参数
         if(StrUtil.isNotBlank(content)) {
-            Map<String, Object> raw = JSONUtil.parseObj(content).getRaw();
-            if (raw != null) values.putAll(raw);
+            try {
+                Map<String, Object> map = OM.readValue(content, MAP_TYPE);
+                if (map != null) values.putAll(map);
+            } catch (JsonProcessingException e) {
+                log.error("解析json内容异常: {}", content);
+                throw new RuntimeException(e);
+            }
         }
 
-//        zeebe.newPublishMessageCommand()
-//                .messageName(type)
-//                .correlationKey(correlationKey)
-//                .variables(values)
-//                .send()
-//                .join();
+        zeebe.newPublishMessageCommand()
+                .messageName(type)
+                .correlationKey(correlationKey)
+                .variables(values)
+                .send()
+                .join();
 
         return Result.success();
     }
