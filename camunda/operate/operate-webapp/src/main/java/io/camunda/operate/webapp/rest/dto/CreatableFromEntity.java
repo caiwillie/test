46a6074caf08bdaee291f7ba3a4ec90154cdf891
蔡尉立 <caiwillie@163.com>
2022-07-19 16:16:04 +0800

    [Bug修复](master): 重构结构

--- camunda/operate/operate-webapp/src/main/java/io/camunda/operate/webapp/rest/dto/CreatableFromEntity.java
+++
@@ -1,8 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package io.camunda.operate.webapp.rest.dto;
-
-public interface CreatableFromEntity<T extends CreatableFromEntity<T, E>, E> {
-    public T fillFrom(E var1);
-}
--- camunda/operate/operate-webapp/src/main/java/io/camunda/operate/webapp/zeebe/operation/ExecutionFinishedListener.java
+++
@@ -1,8 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package io.camunda.operate.webapp.zeebe.operation;
-
-public interface ExecutionFinishedListener {
-    public void onExecutionFinished();
-}
