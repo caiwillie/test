5544d2ddbc4c6ca006b0410b433ca894bbd25598
蔡尉立 <caiwillie@163.com>
2022-07-21 17:37:43 +0800

    [Bug修复](master): 更新场景

---
+++ poc/src/main/java/com/brandnewdata/mop/poc/group/dao/BusinessSceneProcessDao.java
@@ -0,0 +1,16 @@
+package com.brandnewdata.mop.poc.group.dao;
+
+import com.brandnewdata.mop.poc.group.entity.BusinessSceneProcessEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-07-21
+ */
+public interface BusinessSceneProcessDao extends BaseMapper<BusinessSceneProcessEntity> {
+
+}
---
+++ poc/src/main/java/com/brandnewdata/mop/poc/group/entity/BusinessSceneProcessEntity.java
@@ -0,0 +1,39 @@
+package com.brandnewdata.mop.poc.group.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-07-21
+ */
+@Getter
+@Setter
+@TableName("mop_business_scene_process")
+public class BusinessSceneProcessEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private Integer businessSceneId;
+
+    private String processId;
+
+
+    public static final String ID = "id";
+
+    public static final String BUSINESS_SCENE_ID = "business_scene_id";
+
+    public static final String PROCESS_ID = "process_id";
+
+}
