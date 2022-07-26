e4580fa05cb0c43faac4fb0086fa982612d57064
蔡尉立 <caiwillie@163.com>
2022-07-26 16:32:40 +0800

    [新增功能](master): 场景 + 流程支持

---
+++ poc/src/main/java/com/brandnewdata/mop/poc/process/dao/ProcessDeployVersionDao.java
@@ -0,0 +1,16 @@
+package com.brandnewdata.mop.poc.process.dao;
+
+import com.brandnewdata.mop.poc.process.entity.ProcessDeployVersionEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-07-26
+ */
+public interface ProcessDeployVersionDao extends BaseMapper<ProcessDeployVersionEntity> {
+
+}
