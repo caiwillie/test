39d34f13d8e31d36617c4c46e661bfd5dd4427e0
蔡尉立 <caiwillie@163.com>
2022-07-27 17:59:56 +0800

    [新增功能](master): 场景 + 流程支持

--- poc/src/main/java/com/brandnewdata/mop/poc/process/dto/ProcessInstance.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/process/dto/ProcessInstance.java
@@ -5,15 +5,33 @@ import lombok.Data;
 @Data
 public class ProcessInstance {
 
+    /**
+     * 流程 id
+     */
     private String processId;
 
+    /**
+     * 流程版本
+     */
     private String version;
 
+    /**
+     * 流程实例 id
+     */
     private String instanceId;
 
+    /**
+     * 父级流程实例 id
+     */
     private String parentInstanceId;
 
+    /**
+     * 开始时间
+     */
     private String startTime;
 
+    /**
+     * 结束时间
+     */
     private String endTime;
 }
