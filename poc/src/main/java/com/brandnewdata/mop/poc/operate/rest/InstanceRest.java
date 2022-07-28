e76aa070080b48443a7e47249dbdc19e91a63c8a
蔡尉立 <caiwillie@163.com>
2022-07-28 09:18:02 +0800

    [新增功能](master): 场景 + 流程支持

--- poc/src/main/java/com/brandnewdata/mop/poc/operate/rest/InstanceRest.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/operate/rest/InstanceRest.java
@@ -2,13 +2,19 @@ package com.brandnewdata.mop.poc.operate.rest;
 
 import com.brandnewdata.common.webresult.Result;
 import com.brandnewdata.mop.poc.common.dto.Page;
+import com.brandnewdata.mop.poc.operate.service.InstanceService;
 import com.brandnewdata.mop.poc.process.dto.ProcessInstance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class InstanceRest {
+
+    @Resource
+    private InstanceService instanceService;
 
     /**
      * 流程实例分页列表
@@ -21,9 +27,8 @@ public class InstanceRest {
     public Result<Page<ProcessInstance>> page (
             @RequestParam int pageNum,
             @RequestParam int pageSize) {
-        return null;
-
-
+        Page<ProcessInstance> page = instanceService.page(pageNum, pageSize);
+        return Result.OK(page);
     }
 
 }
