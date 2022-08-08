25626b7b4ea6d1b11871edaf539a60438661f3e9
蔡尉立 <caiwillie@163.com>
2022-08-08 17:45:39 +0800

    [新增功能](master): 场景 + 流程支持

--- poc/src/main/java/com/brandnewdata/mop/poc/proxy/rest/ReverseProxyRest.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/proxy/rest/ReverseProxyRest.java
@@ -1,12 +1,10 @@
 package com.brandnewdata.mop.poc.proxy.rest;
 
 import com.brandnewdata.common.webresult.Result;
+import com.brandnewdata.mop.poc.common.dto.Page;
 import com.brandnewdata.mop.poc.proxy.dto.Endpoint;
 import com.brandnewdata.mop.poc.proxy.dto.Proxy;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * API管理相关的接口
@@ -34,8 +32,21 @@ public class ReverseProxyRest {
      * @param id api id
      * @return the result
      */
-    @PostMapping("/rest/reverseProxy/detail")
+    @GetMapping("/rest/reverseProxy/detail")
     public Result<Proxy> detail(@RequestParam Long id) {
+        return null;
+    }
+
+
+    /**
+     * API 分页列表
+     *
+     * @param pageNum  分页码
+     * @param pageSize 分页大小
+     * @return the result
+     */
+    @GetMapping("/rest/reverseProxy/page")
+    public Result<Page<Proxy>> page(@RequestParam int pageNum, @RequestParam int pageSize) {
         return null;
     }
 
@@ -56,8 +67,20 @@ public class ReverseProxyRest {
      * @param id endpoint 详情
      * @return the result
      */
-    @PostMapping("/rest/reverseProxy/detailEndpoint")
+    @GetMapping("/rest/reverseProxy/detailEndpoint")
     public Result<Endpoint> detailEndpoint(@RequestParam Long id) {
+        return null;
+    }
+
+    /**
+     * endpoint 分页列表
+     *
+     * @param pageNum  分页码
+     * @param pageSize 分页大小
+     * @return the result
+     */
+    @GetMapping("/rest/reverseProxy/pageEndpoint")
+    public Result<Endpoint> pageEndpoint(@RequestParam int pageNum, @RequestParam int pageSize) {
         return null;
     }
 
