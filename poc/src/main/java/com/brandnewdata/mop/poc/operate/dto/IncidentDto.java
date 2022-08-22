b01051784ece9707f5d26575a9c79e08d3198b0e
蔡尉立 <caiwillie@163.com>
2022-08-22 10:03:19 +0800

    更新 枚举 和 字典类型

--- poc/src/main/java/com/brandnewdata/mop/poc/operate/dto/IncidentDto.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/operate/dto/IncidentDto.java
@@ -6,16 +6,27 @@ import java.time.OffsetDateTime;
 public class IncidentDto {
 
     public static final String FALLBACK_PROCESS_DEFINITION_NAME = "Unknown process";
+
     private String id;
+
     private ErrorTypeDto errorType;
+
     private String errorMessage;
+
     private String flowNodeId;
+
     private String flowNodeInstanceId;
+
     private String jobId;
+
     private LocalDateTime creationTime;
+
     private boolean hasActiveOperation = false;
+
     private LocalDateTime lastOperation;
+
     private ProcessInstanceReferenceDto rootCauseInstance;
+
     private DecisionInstanceReferenceDto rootCauseDecision;
 
 }
