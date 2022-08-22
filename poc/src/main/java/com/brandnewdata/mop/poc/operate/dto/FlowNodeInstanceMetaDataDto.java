35bc0b8b5889f0b3e5799a4a157b844156443804
蔡尉立 <caiwillie@163.com>
2022-08-22 09:40:04 +0800

    更新 枚举 和 字典类型

--- poc/src/main/java/com/brandnewdata/mop/poc/operate/dto/FlowNodeInstanceMetaDataDto.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/operate/dto/FlowNodeInstanceMetaDataDto.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @Data
 public class FlowNodeInstanceMetaDataDto implements
         FromTwoEntity<FlowNodeInstanceMetaDataDto, FlowNodeInstanceEntity, EventEntity> {
+    private String processInstanceId;
     private String flowNodeId;
     private String flowNodeInstanceId;
     private FlowNodeType flowNodeType;
@@ -30,9 +31,9 @@ public class FlowNodeInstanceMetaDataDto implements
     private OffsetDateTime jobDeadline;
     private Map<String, String> jobCustomHeaders;
 
-
     @Override
     public FlowNodeInstanceMetaDataDto fromEntity(FlowNodeInstanceEntity flowNodeInstanceEntity, EventEntity eventEntity) {
+        this.setProcessInstanceId(String.valueOf(flowNodeInstanceEntity.getProcessInstanceKey()));
         this.setFlowNodeInstanceId(flowNodeInstanceEntity.getId());
         this.setFlowNodeId(flowNodeInstanceEntity.getFlowNodeId());
         this.setFlowNodeType(flowNodeInstanceEntity.getType());
---
+++ poc/src/main/java/com/brandnewdata/mop/poc/operate/service/IncidentService.java
@@ -0,0 +1,16 @@
+package com.brandnewdata.mop.poc.operate.service;
+
+import com.brandnewdata.mop.poc.operate.dto.IncidentDto;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author caiwillie
+ */
+@Service
+public class IncidentService {
+
+    public IncidentDto get() {
+        return null;
+    }
+
+}
