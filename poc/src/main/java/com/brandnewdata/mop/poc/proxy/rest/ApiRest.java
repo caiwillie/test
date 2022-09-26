1100a219fb70f4def655845f04b2dffa0cc449bd
蔡尉立 <caiwillie@163.com>
2022-09-26 15:24:46 +0800

    更新代码

--- poc/src/main/java/com/brandnewdata/mop/poc/proxy/rest/ApiRest.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/proxy/rest/ApiRest.java
@@ -5,6 +5,7 @@ import com.brandnewdata.mop.poc.common.dto.Page;
 import com.brandnewdata.mop.poc.proxy.req.ImportFromFileReq;
 import com.brandnewdata.mop.poc.proxy.req.ProxyReq;
 import com.brandnewdata.mop.poc.proxy.resp.ApiResp;
+import com.brandnewdata.mop.poc.proxy.resp.InspectResp;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -56,18 +57,47 @@ public class ApiRest {
         return null;
     }
 
+    /**
+     * 删除API
+     *
+     * @param proxyReq the proxy req
+     * @return the result
+     */
     @PostMapping("/rest/reverseProxy/delete")
     public Result delete(@RequestBody ProxyReq proxyReq) {
         return null;
     }
 
+    /**
+     * 更改API的状态
+     *
+     * @param proxyReq the proxy req
+     * @return the result
+     */
     @PostMapping("/rest/reverseProxy/changeState")
     public Result changeState(@RequestBody ProxyReq proxyReq) {
         return null;
     }
 
+    /**
+     * 删除Endpoint
+     *
+     * @param req the req
+     * @return the result
+     */
     @PostMapping("/rest/reverseProxy/deleteEndpoint")
     public Result deleteEndpoint(@RequestBody EndpointReq req) {
+        return null;
+    }
+
+    /**
+     * 查看描述文件
+     *
+     * @param proxyReq the proxy req
+     * @return the result
+     */
+    @PostMapping("/rest/reverseProxy/inspect")
+    public Result<InspectResp> inspect(@RequestBody ProxyReq proxyReq) {
         return null;
     }
 
