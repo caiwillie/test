2d4a56ef1839c501ea9e6ef164c007c451694be2
蔡尉立 <caiwillie@163.com>
2022-11-09 17:27:06 +0800

    更新代码

---
+++ poc/src/main/java/com/brandnewdata/mop/poc/scene/dao/SceneLoadDao.java
@@ -0,0 +1,16 @@
+package com.brandnewdata.mop.poc.scene.dao;
+
+import com.brandnewdata.mop.poc.scene.entity.SceneLoadEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-11-09
+ */
+public interface SceneLoadDao extends BaseMapper<SceneLoadEntity> {
+
+}
---
+++ poc/src/main/java/com/brandnewdata/mop/poc/scene/entity/SceneLoadEntity.java
@@ -0,0 +1,58 @@
+package com.brandnewdata.mop.poc.scene.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.sql.Blob;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.ibatis.type.BlobTypeHandler;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-11-09
+ */
+@Getter
+@Setter
+@TableName("mop_scene_load")
+public class SceneLoadEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @TableField(typeHandler = BlobTypeHandler.class)
+    private byte[] zipBytes;
+
+    @TableField(fill = FieldFill.INSERT)
+    private String createBy;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updateBy;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+
+    public static final String ID = "id";
+
+    public static final String ZIP_BYTES = "zip_bytes";
+
+    public static final String CREATE_BY = "create_by";
+
+    public static final String CREATE_TIME = "create_time";
+
+    public static final String UPDATE_BY = "update_by";
+
+    public static final String UPDATE_TIME = "update_time";
+
+}
