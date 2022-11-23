ec598ba80c5f4a0e7edfc6eaab007b4ceb76c1b8
蔡尉立 <caiwillie@163.com>
2022-11-23 09:41:38 +0800

    更新

---
+++ poc/src/main/java/com/brandnewdata/mop/poc/scene/dao/SceneVersionDao.java
@@ -0,0 +1,16 @@
+package com.brandnewdata.mop.poc.scene.dao;
+
+import com.brandnewdata.mop.poc.scene.po.SceneVersionPo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-11-23
+ */
+public interface SceneVersionDao extends BaseMapper<SceneVersionPo> {
+
+}
---
+++ poc/src/main/java/com/brandnewdata/mop/poc/scene/po/SceneVersionPo.java
@@ -0,0 +1,53 @@
+package com.brandnewdata.mop.poc.scene.po;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-11-23
+ */
+@Getter
+@Setter
+@TableName("mop_scene_version")
+public class SceneVersionPo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    private String version;
+
+    private Long sceneId;
+
+    private Integer status;
+
+
+    public static final String ID = "id";
+
+    public static final String CREATE_TIME = "create_time";
+
+    public static final String UPDATE_TIME = "update_time";
+
+    public static final String VERSION = "version";
+
+    public static final String SCENE_ID = "scene_id";
+
+    public static final String STATUS = "status";
+
+}
