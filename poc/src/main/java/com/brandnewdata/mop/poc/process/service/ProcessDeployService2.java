2d5b9402978a6dae3b9d771e23d94daeca9d64db
蔡尉立 <caiwillie@163.com>
2022-11-25 11:18:22 +0800

    更新

--- poc/src/main/java/com/brandnewdata/mop/poc/process/service/ProcessDeployService2.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/process/service/ProcessDeployService2.java
@@ -1,5 +1,7 @@
 package com.brandnewdata.mop.poc.process.service;
 
+import com.brandnewdata.mop.poc.process.dao.ProcessReleaseDeployDao;
+import com.brandnewdata.mop.poc.process.dao.ProcessSnapshotDeployDao;
 import com.brandnewdata.mop.poc.process.dto.ZeebeDeployDto;
 import com.brandnewdata.mop.poc.process.manager.ZeebeClientManager;
 
@@ -7,10 +9,16 @@ public class ProcessDeployService2 implements IProcessDeployService2 {
 
     private final ZeebeClientManager zeebeClientManager;
 
+    private final ProcessSnapshotDeployDao snapshotDeployDao;
 
+    private final ProcessReleaseDeployDao releaseDeployDao;
 
-    public ProcessDeployService2(ZeebeClientManager zeebeClientManager) {
+    public ProcessDeployService2(ZeebeClientManager zeebeClientManager,
+                                 ProcessSnapshotDeployDao snapshotDeployDao,
+                                 ProcessReleaseDeployDao releaseDeployDao) {
         this.zeebeClientManager = zeebeClientManager;
+        this.snapshotDeployDao = snapshotDeployDao;
+        this.releaseDeployDao = releaseDeployDao;
     }
 
     @Override
