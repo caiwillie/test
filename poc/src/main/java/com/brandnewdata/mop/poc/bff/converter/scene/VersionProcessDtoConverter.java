46ec1b648206ba291ccb6e4c91c2c471041fa606
蔡尉立 <caiwillie@163.com>
2022-11-30 14:58:53 +0800

    更新

--- poc/src/main/java/com/brandnewdata/mop/poc/bff/converter/scene/VersionProcessDtoConverter.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/bff/converter/scene/VersionProcessDtoConverter.java
@@ -1,5 +1,6 @@
 package com.brandnewdata.mop.poc.bff.converter.scene;
 
+import cn.hutool.core.lang.Opt;
 import com.brandnewdata.mop.poc.bff.vo.scene.VersionProcessVo;
 import com.brandnewdata.mop.poc.scene.dto.VersionProcessDto;
 
@@ -7,7 +8,7 @@ public class VersionProcessDtoConverter {
 
     public static VersionProcessDto createFrom(VersionProcessVo vo) {
         VersionProcessDto dto = new VersionProcessDto();
-        dto.setId(Long.parseLong(vo.getId()));
+        dto.setId(Opt.ofNullable(vo.getId()).map(Long::parseLong).orElse(null));
         dto.setCreateTime(vo.getCreateTime());
         dto.setUpdateTime(vo.getUpdateTime());
         dto.setVersionId(vo.getVersionId());
