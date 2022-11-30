703cf1bae5ce43ec12739ed79a8b95874c5470df
蔡尉立 <caiwillie@163.com>
2022-11-30 13:37:52 +0800

    更新

--- poc/src/test/java/com/brandnewdata/mop/poc/operate/service/InstanceServiceTest.java
+++
@@ -1,8 +0,0 @@
-package com.brandnewdata.mop.poc.operate.service;
-
-class InstanceServiceTest {
-
-
-}
-
-//Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
