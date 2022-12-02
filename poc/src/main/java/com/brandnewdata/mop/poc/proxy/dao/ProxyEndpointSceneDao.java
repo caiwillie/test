043cd7fcdf0251b3c755e586bd163419abafae05
蔡尉立 <caiwillie@163.com>
2022-12-02 10:45:07 +0800

    更新2

---
+++ poc/src/main/java/com/brandnewdata/mop/poc/proxy/dao/ProxyEndpointSceneDao.java
@@ -0,0 +1,16 @@
+package com.brandnewdata.mop.poc.proxy.dao;
+
+import com.brandnewdata.mop.poc.proxy.po.ProxyEndpointScenePo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-12-01
+ */
+public interface ProxyEndpointSceneDao extends BaseMapper<ProxyEndpointScenePo> {
+
+}
---
+++ poc/src/main/java/com/brandnewdata/mop/poc/proxy/po/ProxyEndpointScenePo.java
@@ -0,0 +1,77 @@
+package com.brandnewdata.mop.poc.proxy.po;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author caiwillie
+ * @since 2022-12-01
+ */
+@Getter
+@Setter
+@TableName("mop_proxy_endpoint_scene")
+public class ProxyEndpointScenePo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    private Long endpointId;
+
+    private Long envId;
+
+    private String envName;
+
+    private Long sceneId;
+
+    private String sceneName;
+
+    private Long versionId;
+
+    private String versionName;
+
+    private String processId;
+
+    private String processName;
+
+
+    public static final String ID = "id";
+
+    public static final String CREATE_TIME = "create_time";
+
+    public static final String UPDATE_TIME = "update_time";
+
+    public static final String ENDPOINT_ID = "endpoint_id";
+
+    public static final String ENV_ID = "env_id";
+
+    public static final String ENV_NAME = "env_name";
+
+    public static final String SCENE_ID = "scene_id";
+
+    public static final String SCENE_NAME = "scene_name";
+
+    public static final String VERSION_ID = "version_id";
+
+    public static final String VERSION_NAME = "version_name";
+
+    public static final String PROCESS_ID = "process_id";
+
+    public static final String PROCESS_NAME = "process_name";
+
+}
