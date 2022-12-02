a48643d2ec0ddd36d8f1b65b83a149888add13ce
蔡尉立 <caiwillie@163.com>
2022-12-02 11:04:10 +0800

    更新2

--- poc/src/main/java/com/brandnewdata/mop/poc/bff/vo/proxy/ProxyEndpointVo.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/bff/vo/proxy/ProxyEndpointVo.java
@@ -27,6 +27,19 @@ public class ProxyEndpointVo {
 
     /**
      * 后端服务配置（通过json.stringify序列化成字符串）
+     *
+     * 如果是集成流，json格式如下
+     * {
+     *     String envName;
+     *     Long sceneId;
+     *     String sceneName;
+     *     Long versionId;
+     *     String versionName;
+     *     String processId;
+     *     String processName;
+     * }
+     *
+     * 如果是第三方服务，内容为输入的baseUrl
      */
     private String backendConfig;
 
