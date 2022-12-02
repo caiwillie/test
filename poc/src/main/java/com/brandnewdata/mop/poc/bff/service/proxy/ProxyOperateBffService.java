077606f6c2e865889f40e2fa00b5296ea4442815
蔡尉立 <caiwillie@163.com>
2022-12-02 15:17:30 +0800

    更新

--- poc/src/main/java/com/brandnewdata/mop/poc/bff/service/proxy/ProxyOperateBffService.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/bff/service/proxy/ProxyOperateBffService.java
@@ -3,19 +3,42 @@ package com.brandnewdata.mop.poc.bff.service.proxy;
 import com.brandnewdata.mop.poc.bff.vo.proxy.operate.ProxyEndpointCallFilter;
 import com.brandnewdata.mop.poc.bff.vo.proxy.operate.ProxyEndpointCallVo;
 import com.brandnewdata.mop.poc.common.dto.Page;
+import com.brandnewdata.mop.poc.proxy.converter.ProxyEndpointDtoConverter;
+import com.brandnewdata.mop.poc.proxy.dto.ProxyDto;
+import com.brandnewdata.mop.poc.proxy.dto.ProxyEndpointDto;
 import com.brandnewdata.mop.poc.proxy.service.IProxyEndpointService2;
+import com.brandnewdata.mop.poc.proxy.service.IProxyService2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ProxyOperateBffService {
 
     private final IProxyEndpointService2 proxyEndpointService;
 
-    public ProxyOperateBffService(IProxyEndpointService2 proxyEndpointService) {
+    private final IProxyService2 proxyService;
+
+    public ProxyOperateBffService(IProxyEndpointService2 proxyEndpointService,
+                                  IProxyService2 proxyService) {
         this.proxyEndpointService = proxyEndpointService;
+        this.proxyService = proxyService;
     }
 
     public Page<ProxyEndpointCallVo> page(ProxyEndpointCallFilter filter) {
+        List<ProxyEndpointDto> proxyEndpointDtoList = proxyEndpointService.fetchAll();
+
+        // 查询关联的proxy
+        List<Long> proxyIdList = proxyEndpointDtoList.stream().map(ProxyEndpointDto::getProxyId).collect(Collectors.toList());
+        Map<Long, ProxyDto> proxyDtoMap = proxyService.fetchById(proxyIdList);
+
+        // 更新proxy信息
+        for (ProxyEndpointDto proxyEndpointDto : proxyEndpointDtoList) {
+            ProxyDto proxyDto = proxyDtoMap.get(proxyEndpointDto.getProxyId());
+            ProxyEndpointDtoConverter.updateFrom(proxyEndpointDto, proxyDto);
+        }
 
     }
 
--- poc/src/main/java/com/brandnewdata/mop/poc/proxy/converter/ProxyEndpointDtoConverter.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/proxy/converter/ProxyEndpointDtoConverter.java
@@ -2,6 +2,7 @@ package com.brandnewdata.mop.poc.proxy.converter;
 
 import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.core.lang.Opt;
+import com.brandnewdata.mop.poc.proxy.dto.ProxyDto;
 import com.brandnewdata.mop.poc.proxy.dto.ProxyEndpointDto;
 import com.brandnewdata.mop.poc.proxy.po.ProxyEndpointPo;
 
@@ -28,5 +29,10 @@ public class ProxyEndpointDtoConverter {
         target.setBackendType(dto.getBackendType());
         target.setBackendConfig(dto.getBackendConfig());
         target.setTag(dto.getTag());
+    }
+
+    public static void updateFrom(ProxyEndpointDto target, ProxyDto dto) {
+        target.setProxyName(dto.getName());
+        target.setProxyVersion(dto.getVersion());
     }
 }
