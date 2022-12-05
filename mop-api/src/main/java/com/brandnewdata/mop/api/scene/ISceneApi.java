9b61c16f4aff6a53b2d82b469648d061752e4e9b
蔡尉立 <caiwillie@163.com>
2022-12-05 14:26:45 +0800

    更新

--- mop-api/src/main/java/com/brandnewdata/mop/api/scene/ISceneApi.java
+++ mop-api/src/main/java/com/brandnewdata/mop/api/scene/ISceneApi.java
@@ -1,8 +1,8 @@
 package com.brandnewdata.mop.api.scene;
 
 import com.brandnewdata.common.webresult.Result;
-import com.brandnewdata.mop.api.scene.dto.SceneQuery;
 import com.brandnewdata.mop.api.scene.dto.SceneDto;
+import com.brandnewdata.mop.api.scene.dto.SceneQuery;
 import com.brandnewdata.mop.api.scene.dto.VersionProcessStartDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
