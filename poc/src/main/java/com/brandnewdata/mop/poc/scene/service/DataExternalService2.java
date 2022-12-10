166e590d7336136fe322d061ff98c4cdbf97d902
蔡尉立 <caiwillie@163.com>
2022-12-10 14:37:56 +0800

    更新

--- poc/src/main/java/com/brandnewdata/mop/poc/scene/service/DataExternalService2.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/scene/service/DataExternalService2.java
@@ -2,9 +2,13 @@ package com.brandnewdata.mop.poc.scene.service;
 
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.collection.ListUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.lang.Assert;
-import cn.hutool.core.stream.StreamUtil;
+import cn.hutool.core.lang.Opt;
+import cn.hutool.core.map.MapUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.ZipUtil;
 import com.brandnewdata.mop.poc.constant.SceneConst;
 import com.brandnewdata.mop.poc.process.manager.ConnectorManager;
 import com.brandnewdata.mop.poc.process.manager.dto.ConfigInfo;
@@ -15,12 +19,17 @@ import com.brandnewdata.mop.poc.process.util.ProcessUtil;
 import com.brandnewdata.mop.poc.scene.bo.export.ConfigExportBo;
 import com.brandnewdata.mop.poc.scene.bo.export.ConnectorExportBo;
 import com.brandnewdata.mop.poc.scene.bo.export.ProcessExportBo;
+import com.brandnewdata.mop.poc.scene.dto.SceneDto2;
 import com.brandnewdata.mop.poc.scene.dto.SceneVersionDto;
 import com.brandnewdata.mop.poc.scene.dto.SceneVersionExportDto;
 import com.brandnewdata.mop.poc.scene.dto.VersionProcessDto;
+import com.dxy.library.json.jackson.JacksonUtil;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -29,15 +38,22 @@ import java.util.Map;
 @Service
 public class DataExternalService2 implements IDataExternalService2 {
 
+    private static final String FILENAME__PROCESS_DEFINITION = "process_definition.json";
+
+    private static final String FILENAME__CONFIG = "config.json";
+
+    private final ISceneService2 sceneService;
     private final ISceneVersionService sceneVersionService;
 
     private final IVersionProcessService versionProcessService;
 
     private final ConnectorManager connectorManager;
 
-    public DataExternalService2(ISceneVersionService sceneVersionService,
+    public DataExternalService2(ISceneService2 sceneService,
+                                ISceneVersionService sceneVersionService,
                                 IVersionProcessService versionProcessService,
                                 ConnectorManager connectorManager) {
+        this.sceneService = sceneService;
         this.sceneVersionService = sceneVersionService;
         this.versionProcessService = versionProcessService;
         this.connectorManager = connectorManager;
@@ -47,6 +63,10 @@ public class DataExternalService2 implements IDataExternalService2 {
     public SceneVersionExportDto export(Long versionId, List<String> processIdList) {
         SceneVersionDto sceneVersionDto = sceneVersionService.fetchOneByIdAndCheckStatus(versionId, new int[]{SceneConst.SCENE_VERSION_STATUS__RUNNING,
                 SceneConst.SCENE_VERSION_STATUS__STOPPED});
+
+        Long sceneId = sceneVersionDto.getSceneId();
+        SceneDto2 sceneDto = sceneService.fetchById(ListUtil.of(sceneId)).get(sceneId);
+
         List<VersionProcessDto> versionProcessDtoList = versionProcessService
                 .fetchListByVersionId(ListUtil.of(sceneVersionDto.getId()), false).get(versionId);
 
@@ -80,9 +100,15 @@ public class DataExternalService2 implements IDataExternalService2 {
            connectorExportBo.setConfigurations(ListUtil.toList(configExportBoMap.values()));
         });
 
+        fileMap.put(FILENAME__PROCESS_DEFINITION, createTempFile(JacksonUtil.to(processExportBoMap.values())));
 
-        // 流程配置
-        return null;
+        fileMap.put(FILENAME__CONFIG, createTempFile(JacksonUtil.to(connectorConfigMap.keySet())));
+        File dir = createTempDir(fileMap, true);
+        File zip = ZipUtil.zip(dir);
+
+        SceneVersionExportDto ret = new SceneVersionExportDto();
+        ret.setFileName(StrUtil.format("【场景导出】{}-{}.zip", sceneDto.getName(), sceneVersionDto.getVersion()));
+        byte[] bytes = IoUtil.readBytes(FileUtil.getInputStream(zip));
     }
 
     private void parseConfig(Map<ConnectorExportBo, Map<String, ConfigExportBo>> configMap, Map<String, String> configIdMap) {
@@ -124,6 +150,41 @@ public class DataExternalService2 implements IDataExternalService2 {
 
         }
 
+    }
+
+    private static File createTempFile(String content) {
+        Path path = null;
+        try {
+            path = Files.createTempFile("", "");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        File file = path.toFile();
+        FileUtil.writeUtf8String(content, file);
+        return file;
+    }
+
+    private static File createTempDir(Map<String, File> srcMap, boolean deleteSrc) {
+        Path path = null;
+        try {
+            path = Files.createTempDirectory("");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        File dir = path.toFile();
+
+        srcMap = Opt.ofNullable(srcMap).orElse(MapUtil.<String, File>empty());
+        for (Map.Entry<String, File> entry : srcMap.entrySet()) {
+            String fileName = entry.getKey();
+            File src = entry.getValue();
+            File target = new File(dir, fileName);
+            if(deleteSrc) {
+                FileUtil.move(src, target, true);
+            } else {
+                FileUtil.copy(src, target, true);
+            }
+        }
+        return dir;
     }
 
 
