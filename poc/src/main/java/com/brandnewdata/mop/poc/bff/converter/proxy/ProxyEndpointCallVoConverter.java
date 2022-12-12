4e5545cd29643b64a79f9255abf03cae995679ce
蔡尉立 <caiwillie@163.com>
2022-12-12 16:56:04 +0800

    更新

--- poc/src/main/java/com/brandnewdata/mop/poc/bff/converter/proxy/ProxyEndpointCallVoConverter.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/bff/converter/proxy/ProxyEndpointCallVoConverter.java
@@ -1,13 +1,29 @@
 package com.brandnewdata.mop.poc.bff.converter.proxy;
 
+import cn.hutool.core.util.StrUtil;
 import com.brandnewdata.mop.poc.bff.vo.proxy.operate.ProxyEndpointCallVo;
 import com.brandnewdata.mop.poc.proxy.dto.ProxyEndpointCallDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProxyEndpointCallVoConverter {
 
     public static ProxyEndpointCallVo createFrom(ProxyEndpointCallDto dto) {
         ProxyEndpointCallVo vo = new ProxyEndpointCallVo();
         vo.setId(String.valueOf(dto.getId()));
+        vo.setCreateTime(dto.getCreateTime());
+        vo.setUpdateTime(dto.getUpdateTime());
+        // 设置source
+        List<String> sourceList = new ArrayList<>();
+        if(StrUtil.isNotBlank(dto.getIpAddress())) {
+            sourceList.add(dto.getIpAddress());
+        }
+        if(StrUtil.isNotBlank(dto.getUserAgent())) {
+            sourceList.add(dto.getUserAgent());
+        }
+        vo.setSource(StrUtil.join(" / ",sourceList));
+
         vo.setEndpointId(dto.getEndpointId());
         vo.setProxyId(dto.getProxyId());
         vo.setProxyName(dto.getProxyName());
