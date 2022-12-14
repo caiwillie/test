719d0d5cda2584242915db8f300a091979a778d0
蔡尉立 <caiwillie@163.com>
2022-12-14 13:53:33 +0800

    更新

---
+++ poc/src/main/java/com/brandnewdata/mop/poc/operate/cache/ProcessInstanceCache.java
@@ -0,0 +1,56 @@
+package com.brandnewdata.mop.poc.operate.cache;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import com.brandnewdata.mop.poc.env.dto.EnvDto;
+import com.brandnewdata.mop.poc.env.service.IEnvService;
+import com.brandnewdata.mop.poc.operate.dto.ListViewProcessInstanceDto;
+import com.brandnewdata.mop.poc.operate.manager.ElasticsearchManager;
+import com.caiwillie.util.cache.ScheduleScanEsCache;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.cache.Cache;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+@Component
+public class ProcessInstanceCache {
+
+    private final Map<Long, ScheduleScanEsCache<String, ListViewProcessInstanceDto>> cacheMap = new HashMap<>();
+    private final ElasticsearchManager elasticsearchManager;
+    private final IEnvService envService;
+
+    public ProcessInstanceCache(ElasticsearchManager elasticsearchManager, IEnvService envService) {
+        this.elasticsearchManager = elasticsearchManager;
+        this.envService = envService;
+    }
+
+    private void init() {
+        List<EnvDto> envDtoList = envService.fetchEnvList();
+        for (EnvDto envDto : envDtoList) {
+            Long id = envDto.getId();
+            ElasticsearchClient client = elasticsearchManager.getByEnvId(id);
+
+            BoolQuery filter = new BoolQuery.Builder()
+                    .must(new Query.Builder().term(t -> t.field("joinRelation").value("processInstance")).build())
+                    .build();
+
+            ScheduleScanEsCache<String, ListViewProcessInstanceDto> cache = new ScheduleScanEsCache<>("operate-list-view-1.3.0_alias", "id",
+                    "startDate", client, "0/4 * * * * ?", filter, getConsume());
+
+            cacheMap.put(id, cache);
+        }
+    }
+
+    public Map<String, ListViewProcessInstanceDto> asMap(Long envId) {
+        return cacheMap.get(envId).asMap();
+    }
+
+    private BiConsumer<List<ObjectNode>, Cache<String, ListViewProcessInstanceDto>> getConsume() {
+        return null;
+    }
+}
--- poc/src/test/java/com/brandnewdata/mop/poc/es/EsTest.java
+++ poc/src/test/java/com/brandnewdata/mop/poc/es/EsTest.java
@@ -1,14 +1,28 @@
 package com.brandnewdata.mop.poc.es;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.LocalDateTimeUtil;
+import cn.hutool.core.thread.ThreadUtil;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.FieldSort;
+import co.elastic.clients.elasticsearch._types.SortOptions;
+import co.elastic.clients.elasticsearch._types.SortOrder;
 import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.ExistsQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.RangeQuery;
 import co.elastic.clients.elasticsearch.cluster.HealthResponse;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.json.JsonData;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.brandnewdata.mop.poc.operate.schema.template.ListViewTemplate;
 import com.brandnewdata.mop.poc.util.HttpHostUtil;
+import com.caiwillie.util.cache.ElasticsearchUtil;
 import com.caiwillie.util.cache.ScheduleScanEsCache;
 import com.dxy.library.json.jackson.JacksonUtil;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -19,7 +33,12 @@ import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 public class EsTest {
@@ -47,18 +66,44 @@ public class EsTest {
         System.out.println(health);
     }
 
+    @SneakyThrows
     @Test
-    void test() {
-        BoolQuery filter = new BoolQuery.Builder()
-                .must(new Query.Builder().term(t -> t.field("joinRelation").value("processInstance")).build())
-                .build();
+    void test1() {
 
-        new ScheduleScanEsCache("operate-list-view-1.3.0_alias", "_id",
+        OffsetDateTime date = OffsetDateTime.now();
+        List<Query> mustList = new ArrayList<>();
+        mustList.add(new Query.Builder().range(new RangeQuery.Builder().field("startDate")
+                .format(DatePattern.UTC_SIMPLE_MS_PATTERN)
+                .timeZone("+08:00")
+                .gt(JsonData.of("2000-01-01T00:00:00.000")).build()).build());
+        mustList.add(new Query.Builder().exists(new ExistsQuery.Builder().field("id").build()).build());
+
+        SearchRequest request = new SearchRequest.Builder()
+                .index("operate-list-view-1.3.0_alias")
+                .query(new Query.Builder().bool(new BoolQuery.Builder().must(mustList).build()).build())
+                .build();
+        List<ObjectNode> entities = ElasticsearchUtil.scrollAll(client, request, ObjectNode.class);
+
+        return;
+    }
+
+    @Test
+    void test2() {
+
+
+        new ScheduleScanEsCache("operate-list-view-1.3.0_alias", "id",
                 "startDate", client, "0/4 * * * * ?", filter,
                 (BiConsumer<List<ObjectNode>, Cache>) (objectNodes, cache) -> {
                     return;
                 });
+        ThreadUtil.sleep(1000000);
     }
 
+    @Test
+    void test3() {
+        String format = DateUtil.format(DateUtil.date(), DatePattern.UTC_MS_WITH_XXX_OFFSET_PATTERN);
+        DateTime parse = DateUtil.parse("2022-12-14T13:25:55.507+08:00", DatePattern.UTC_MS_WITH_XXX_OFFSET_PATTERN);
+        return;
+    }
 
 }
