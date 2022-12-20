d8e4b128ff7a0e42f29d3370861410654b0dc010
蔡尉立 <caiwillie@163.com>
2022-12-20 17:12:04 +0800

    更新

--- mop-api/src/main/java/com/brandnewdata/mop/api/bff/home/IHomeApi.java
+++ mop-api/src/main/java/com/brandnewdata/mop/api/bff/home/IHomeApi.java
@@ -3,14 +3,17 @@ package com.brandnewdata.mop.api.bff.home;
 import com.brandnewdata.mop.api.bff.home.dto.HomeSceneDto;
 import com.brandnewdata.mop.api.bff.home.dto.HomeStaticOverviewCountDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @FeignClient(name = "poc", contextId = "bffHomeApi")
 public interface IHomeApi {
 
+    @RequestMapping("/api/bff/home/staticOverviewCount")
     HomeStaticOverviewCountDto staticOverviewCount();
 
+    @RequestMapping("/api/bff/home/sceneList")
     List<HomeSceneDto> sceneList();
 
 }
