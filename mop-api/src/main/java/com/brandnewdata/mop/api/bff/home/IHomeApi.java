687d7aeda888fa69466905786ff7c8bd50733c28
蔡尉立 <caiwillie@163.com>
2022-12-21 09:59:19 +0800

    更新

--- mop-api/src/main/java/com/brandnewdata/mop/api/bff/home/IHomeApi.java
+++ mop-api/src/main/java/com/brandnewdata/mop/api/bff/home/IHomeApi.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@FeignClient(name = "poc", contextId = "bffHomeApi")
+// @FeignClient(name = "poc", contextId = "bffHomeApi")
 public interface IHomeApi {
 
     @RequestMapping("/api/bff/home/staticOverviewCount")
