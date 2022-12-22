7903f76daf313c9084eba2317b5ad8434c4cd4fd
蔡尉立 <caiwillie@163.com>
2022-12-22 17:04:33 +0800

    更新

--- poc/src/main/java/com/brandnewdata/mop/poc/operate/cache/ProcessInstanceCache.java
+++ poc/src/main/java/com/brandnewdata/mop/poc/operate/cache/ProcessInstanceCache.java
@@ -5,21 +5,15 @@ import cn.hutool.cron.Scheduler;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
 import com.brandnewdata.mop.poc.env.dto.EnvDto;
 import com.brandnewdata.mop.poc.env.service.IEnvService;
 import com.brandnewdata.mop.poc.operate.dto.ListViewProcessInstanceDto;
 import com.brandnewdata.mop.poc.operate.dto.ProcessInstanceStateDto;
 import com.brandnewdata.mop.poc.operate.manager.ElasticsearchManager;
-import com.brandnewdata.mop.poc.util.HttpHostUtil;
 import com.caiwillie.util.cache.ScheduleScanEsCache;
-import com.dxy.library.json.jackson.JacksonUtil;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.cache.Cache;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
+import io.camunda.operate.dto.ProcessInstanceState;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -41,18 +35,6 @@ public class ProcessInstanceCache {
     }
 
     private void init() {
-        HttpHost httpHost = HttpHostUtil.createHttpHost("es-connector1.basic.dev.brandnewdata.com:8080");
-
-        // Create the low-level client
-        RestClient restClient = RestClient.builder(new HttpHost[]{httpHost}).build();
-
-        // Create the transport with a Jackson mapper
-        ElasticsearchTransport transport = new RestClientTransport(
-                restClient, new JacksonJsonpMapper(JacksonUtil.getObjectMapper()));
-
-        // And create the API client
-        // ElasticsearchClient client = new ElasticsearchClient(transport);
-
         Scheduler scheduler = new Scheduler();
         scheduler.setMatchSecond(true);
         scheduler.schedule("0/10 * * * * ?", (Runnable) () -> {
@@ -71,12 +53,6 @@ public class ProcessInstanceCache {
                 ScheduleScanEsCache<String, ListViewProcessInstanceDto> cache = new ScheduleScanEsCache<>(
                         "operate-list-view-1.3.0_alias", "id",
                         "startDate", client, "0/4 * * * * ?", filter, getConsume());
-
-/*
-                ScheduleScanEsCache<String, ListViewProcessInstanceDto> cache = new ScheduleScanEsCache<>(
-                        "operate-list-view-1.3.0_alias", "id",
-                        "startDate", client, "0/4 * * * * ?", filter, getConsume());
-*/
 
                 cacheMap.put(id, cache);
             }
@@ -104,7 +80,14 @@ public class ProcessInstanceCache {
         dto.setProcessId(objectNode.path("processDefinitionKey").longValue());
         dto.setStartDate(DateUtil.parse(objectNode.path("startDate").textValue()).toLocalDateTime());
         dto.setBpmnProcessId(objectNode.path("bpmnProcessId").textValue());
-        dto.setState(ProcessInstanceStateDto.valueOf(objectNode.path("state").textValue()));
+        ProcessInstanceState state = ProcessInstanceState.valueOf(objectNode.path("state").textValue());
+        boolean incident = objectNode.path("incident").booleanValue();
+        if(state == ProcessInstanceState.ACTIVE && incident) {
+            dto.setState(ProcessInstanceStateDto.INCIDENT);
+        } else {
+            dto.setState(ProcessInstanceStateDto.getState(state));
+        }
+
         return dto;
     }
 }
